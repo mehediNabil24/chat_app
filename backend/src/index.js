5b3628833df7fe63c1d9359d5ae4ab3ee0dfec26
mehediNabil24 <mehedinabil13@gmail.com>
2025-12-23 15:01:32 +0600

    added signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 dotenv.config()
+
 const app = express();
 
 const PORT = process.env.PORT
+
+app.use(express.json()); 
 
 app.use("/api/auth",authRoutes)
 
  app.listen(5001, ()=>{
     console.log("server is running on PORT:"+PORT);
+    connectDB();
  });
