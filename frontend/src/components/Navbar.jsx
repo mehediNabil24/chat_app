1abece33a0d7aa73553ff513551bb8512b77d614
mehediNabil <mehedinabil13@gmail.com>
2025-12-28 13:59:12 +0600

    added text

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,11 +14,14 @@ const Navbar = () => {
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
-            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold">Sadi </h1>
+              <h1 className="text-lg font-bold">Sadi💖</h1>
             </Link>
           </div>
 
@@ -41,7 +44,10 @@ const Navbar = () => {
                   <span className="hidden sm:inline">Profile</span>
                 </Link>
 
-                <button className="flex gap-2 cursor-pointer items-center" onClick={logout}>
+                <button
+                  className="flex gap-2 cursor-pointer items-center"
+                  onClick={logout}
+                >
                   <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
